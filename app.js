a84a7b9e74d1a185f3993e7242769aae522528e6
Dang Yen Linh <nhacuaphuong@gmail.com>
2022-09-22 16:49:50 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
@@ -7,6 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
